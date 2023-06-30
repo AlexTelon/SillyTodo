33f86d5c5233307e9de1420d3035b4ee7c2548da
Alex Telon <alex.telon@gmail.com>
2023-06-30 21:14:52 +0200

    more golf and resonable simplifications

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-T = ['email john', 'buy milk', 'buy eggs']
+T = []
 import sys
 
-def save():
-    content = open('main.py').read().splitlines()[1:]
-    with open('main.py', 'w') as f:
-        f.writelines('\n'.join([f'T = {repr(T)}'] + content))
-
 match sys.argv[1:]:
-    case ['remove', i]:              del T[int(i)-1];save()
-    case ['list']:                   print('\n'.join(f'{i}: {item}'for i, item in enumerate(T, start=1)))
-    case ['add', *items] | [*items]: T.append(' '.join(items));save()
-    case _:                          print('Unknown command')
+    case ['remove', i]: del T[int(i)]
+    case ['list']:      print('\n'.join(f'{i}: {item}'for i, item in enumerate(T)))
+    case [*items]:      T += [' '.join(items)]
+
+content = open('main.py').read().splitlines()[1:]
+with open('main.py', 'w') as f:
+    f.writelines('\n'.join([f'T = {repr(T)}'] + content))
