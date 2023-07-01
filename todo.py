8f167e5218f9a3f810dabc35abcc80e73d6d1d68
Alex Telon <alex.telon@gmail.com>
2023-07-01 21:43:26 +0200

    Really nice version of todo
    If we dont restrict ourselves to one file the solution is greatly
    simplified.
    
    db.py is simply just `T = []`
    todo.py then modifies that accordingly.

---
+++ todo.py
@@ -0,0 +1,9 @@
+from db import T
+import sys
+
+match sys.argv[1:]:
+    case ['remove', i]: del T[int(i)]
+    case ['list']:      print('\n'.join(f'{i}: {item}'for i, item in enumerate(T)))
+    case [*args]:       T.append(' '.join(args))
+
+open('db.py','w').write(f'T = {repr(T)}')
