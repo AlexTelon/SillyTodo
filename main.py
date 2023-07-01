bc2e98103c780c36ba5576a5c4f8085b5093742c
Alex Telon <alex.telon@gmail.com>
2023-07-01 16:09:25 +0200

    Golfed the file writing

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import sys
 match sys.argv[1:]:
     case ['remove', i]: del T[int(i)]
     case ['list']:      print('\n'.join(f'{i}: {item}'for i, item in enumerate(T)))
+    case []:            pass
     case [*items]:      T += [' '.join(items)]
 
-content = open('main.py').read().splitlines()[1:]
-with open('main.py', 'w') as f:
-    f.write('\n'.join([f'T = {repr(T)}'] + content))
+f = open('main.py','r+')
+content = f.read().splitlines()[1:]
+f.truncate(0)
+f.seek(0)
+f.write(f'T = {repr(T)}\n' + '\n'.join(content))
