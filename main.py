047a15d0ef9e7a94612cfa0e8b1f37c85560ff25
Alex Telon <alex.telon@gmail.com>
2023-07-01 20:20:34 +0200

    preparing for more changes

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 T = []
 import sys
 
+f = open('main.py','r+')
+content = f.read().splitlines()[1:]
+f.truncate(0)
+f.seek(0)
+
 match sys.argv[1:]:
     case ['remove', i]: del T[int(i)]
     case ['list']:      print('\n'.join(f'{i}: {item}'for i, item in enumerate(T)))
     case []:            pass
     case [*items]:      T += [' '.join(items)]
 
-f = open('main.py','r+')
-content = f.read().splitlines()[1:]
-f.truncate(0)
-f.seek(0)
-f.write(f'T = {repr(T)}\n' + '\n'.join(content))
+f.write(f'T = {repr(T)}\n')
+f.write('\n'.join(content))
